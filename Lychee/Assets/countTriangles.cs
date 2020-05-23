23a64b290408c7de62ddbe6419476b0f94e6640c
roseate8 <roseate8@users.noreply.github.com>
2020-05-23 22:50:00 +0530

    meshInfo script added

---
+++ Lychee/Assets/countTriangles.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class countTriangles : MonoBehaviour
+{
+    private int vertexCount;
+    private int submeshCount;
+    private int triangleCount;
+    private int vcount;
+    private int subcount;
+    private int tcount;
+    [SerializeField]
+    private GameObject obj;
+
+    // [MenuItem ("Tools/Mesh Info")]
+    // static void Init ()
+    // {
+    //     // Get existing open window or if none, make a new one:
+    //     countTriangles window = (countTriangles)EditorWindow.GetWindow(typeof(countTriangles));
+    //     window.titleContent.text = "Mesh Info";
+    // }
+
+    // void OnSelectionChange()
+    // {
+    //     Repaint();
+    // }
+    void Start()
+    {
+        vcount = obj.GetComponent<MeshFilter>().sharedMesh.vertexCount;
+        subcount = obj.GetComponent<MeshFilter>().sharedMesh.triangles.Length/3;
+        tcount = obj.GetComponent<MeshFilter>().sharedMesh.subMeshCount;
+        Debug.Log("Vertices: " + vcount);
+        Debug.Log("Triangles: " + tcount);
+        Debug.Log("Submeshes: " + subcount);
+    }
+
+
+
+    void OnGUI()
+    {
+        if(Selection.activeGameObject && Selection.activeGameObject.GetComponent<MeshFilter>())
+        {
+
+            vertexCount = Selection.activeGameObject.GetComponent<MeshFilter>().sharedMesh.vertexCount;
+            triangleCount = Selection.activeGameObject.GetComponent<MeshFilter>().sharedMesh.triangles.Length/3;
+            submeshCount = Selection.activeGameObject.GetComponent<MeshFilter>().sharedMesh.subMeshCount;
+
+            EditorGUILayout.LabelField(Selection.activeGameObject.name);
+            EditorGUILayout.LabelField("Vertices: ", vertexCount.ToString());
+            EditorGUILayout.LabelField("Triangles: ", triangleCount.ToString());
+            EditorGUILayout.LabelField("SubMeshes: ", submeshCount.ToString());
+        }
+    }
+}
