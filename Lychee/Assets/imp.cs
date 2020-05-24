5f51a10d097df0faeeeec2d5d57ad4543e74fc41
roseate8 <roseate8@users.noreply.github.com>
2020-05-24 18:45:40 +0530

    check flat wireframe shading

---
+++ Lychee/Assets/imp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class imp : MonoBehaviour
+{
+    public Shader flatWireframe;
+	public Material wireframe;
+   // Use this for initialization
+    void Start()
+    {
+        Mesh holderMesh = new Mesh();
+        ObjImporter newMesh = new ObjImporter();
+        holderMesh = newMesh.ImportFile("E:/Downloads/card.obj");
+
+        MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
+        MeshFilter filter = gameObject.AddComponent<MeshFilter>();
+        Material wireframe = new Material(flatWireframe);
+        filter.mesh = holderMesh;
+        Renderer rend = GetComponent<Renderer>();
+        if (rend != null){
+            rend.material = wireframe;
+        }
+    }
+}
