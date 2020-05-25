b8cf406872031249cd37e81bf8d0b9db871b5b46
roseate8 <roseate8@users.noreply.github.com>
2020-05-25 22:49:12 +0530

    slider added
    Check demo scene

--- Lychee/Assets/subdivision/Subdiv/Demo/Scripts/GPUSubdivTest.cs
+++ Lychee/Assets/subdivision/Subdiv/Demo/Scripts/GPUSubdivTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Subdiv.Demo
 {
@@ -11,15 +12,35 @@ namespace Subdiv.Demo
         [SerializeField] protected ComputeShader subdivCompute;
         [SerializeField, Range(1, 4)] protected int details = 1;
         [SerializeField] bool weld = false;
+        MeshFilter filter;
+        Mesh source;
+        public Slider subdivisionLevel;
+        public Text myText;
+        // public bool isApplybutton;
+        public Button yourButton;
 
-        void Start()
+		void Start()
         {
-            var filter = GetComponent<MeshFilter>();
-            var source = filter.mesh;
+            // Button btn = yourButton.GetComponent<Button>();
+            filter = GetComponent<MeshFilter>();
+            source = filter.mesh;
+            // StartSubdivion();
+            yourButton.onClick.AddListener(StartSubdivion);
+
+        }        
+        void Update() {
+            myText.text = "Subdivison Level: " + subdivisionLevel.value;
+            details = (int)subdivisionLevel.value;
+            // StartSubdivion(filter, source);
+            // Button btn = yourButton.GetComponent<Button>(); 
+            // yourButton.onClick.AddListener(StartSubdivion);
+
+        }
+        public void StartSubdivion()
+        {
             var mesh = GPUSubdivisionSurface.Subdivide(subdivCompute, SubdivisionSurface.Weld(source, float.Epsilon, source.bounds.size.x), details, weld);
             filter.sharedMesh = mesh;
         }
-        
     }
 
 }
