a49c142f8fd04e12a73bda76755947b6babfa2f3
roseate8 <roseate8@users.noreply.github.com>
2020-05-26 23:26:21 +0530

    import object code update
    adds a mesh collider to the imported object *
    may need to change depending on the performance.
    
    mouse pointer updates

--- Lychee/Assets/Scripts/imp.cs
+++ Lychee/Assets/Scripts/imp.cs
@@ -2,17 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.IO;
+
 
 public class imp : MonoBehaviour
 {
     public Shader flatWireframe;
+    public string modelPath = "E:/Downloads/card.obj";
 	public Material wireframe;
-   // Use this for initialization
+
+//  This script imports a new model in .obj format
+//  enter the path for the object in the holdermesh variable
+
     void Start()
     {
+        // Debug.Log(Directory.Exists(modelPath));
+
         Mesh holderMesh = new Mesh();
         ObjImporter newMesh = new ObjImporter();
-        holderMesh = newMesh.ImportFile("E:/Downloads/card.obj");
+        holderMesh = newMesh.ImportFile(modelPath);
+
+        //add mesh collider to the new imported object and bake it according to its mesh
+
+        MeshCollider colliderMesh = gameObject.AddComponent<MeshCollider>();
+        colliderMesh.convex = true;
+        colliderMesh.sharedMesh = holderMesh;
+
+        //adding a wireframe renderer
 
         MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
         MeshFilter filter = gameObject.AddComponent<MeshFilter>();
---
+++ Lychee/Assets/mousePointerInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mousePointerInfo : MonoBehaviour
+{
+    [SerializeField]
+    private string selectableTag = "Selectable";
+    Vector3 pointerPosition = -Vector3.one;
+    
+    void FixedUpdate()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if(selection.CompareTag(selectableTag))
+            {
+                pointerPosition = hit.point;
+                Debug.Log(pointerPosition);   
+            }
+        }   
+    }
+}
