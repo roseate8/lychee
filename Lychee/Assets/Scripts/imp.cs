eca19a4f4de5b8631d70a159c371e996bafaf2ea
roseate8 <roseate8@users.noreply.github.com>
2020-05-27 14:42:08 +0530

    ...

--- Lychee/Assets/Scripts/imp.cs
+++ Lychee/Assets/Scripts/imp.cs
@@ -10,7 +10,7 @@ public class imp : MonoBehaviour
     public Shader flatWireframe;
     public string modelPath = "E:/Downloads/card.obj";
 	public Material wireframe;
-
+    public MeshFilter filter;
 //  This script imports a new model in .obj format
 //  enter the path for the object in the holdermesh variable
 
@@ -28,15 +28,27 @@ public class imp : MonoBehaviour
         colliderMesh.convex = true;
         colliderMesh.sharedMesh = holderMesh;
 
+        // capsule collider
+        // CapsuleCollider colliderMesh = gameObject.AddComponent<CapsuleCollider>();
+        
         //adding a wireframe renderer
 
         MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
-        MeshFilter filter = gameObject.AddComponent<MeshFilter>();
+        filter = gameObject.AddComponent<MeshFilter>();
         Material wireframe = new Material(flatWireframe);
         filter.mesh = holderMesh;
         Renderer rend = GetComponent<Renderer>();
         if (rend != null){
             rend.material = wireframe;
         }
+
+        //aligning the camera to fit the screen
+
+        // Vector3 xyz = filter.GetComponent<MeshFilter>().mesh.bounds.size;
+        // float distance = Mathf.Max(xyz.x, xyz.y, xyz.z);
+        // distance /= (2.0f * Mathf.Tan(0.5f * mainCam.fieldOfView * Mathf.Deg2Rad));
+        // // Move camera in -z-direction; change '2.0f' to your needs
+        // mainCam.transform.position = new Vector3(mainCam.transform.position.x, mainCam.transform.position.y, -distance * 2.0f);
+
     }
 }
---
+++ Lychee/Assets/Scripts/rescaleCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rescaleCamera : MonoBehaviour
+{
+    [SerializeField] Camera mainCam;
+    imp importScript;
+    // [SerializeField] private MeshFilter importedObject;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+       
+    //     if(importScript.isImported)
+    //     {
+    //         // Vector3 xyz = importedObject.GetComponent<MeshFilter>().mesh.bounds.size;
+    //         Vector3 xyz = importScript.filter.GetComponent<MeshFilter>().mesh.bounds.size;
+    //         float distance = Mathf.Max(xyz.x, xyz.y, xyz.z);
+    //         distance /= (2.0f * Mathf.Tan(0.5f * mainCam.fieldOfView * Mathf.Deg2Rad));
+    //         // Move camera in -z-direction; change '2.0f' to your needs
+    //         mainCam.transform.position = new Vector3(mainCam.transform.position.x, mainCam.transform.position.y, -distance * 2.0f);
+    //         importScript.isImported = false;
+    //     }
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
